8f54c581094c103a046e55c3c0d8a94bfc11513b
Robert de Bock <robert@meinit.nl>
2022-09-20 16:32:05 +0200

    Add a description for output.

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "public_ip" {
-  value = aws_instance.default.public_ip
+  description = "The public IP of the instance."
+  value       = aws_instance.default.public_ip
 }
 
 output "ssh_connect_string" {
-  value = "ssh -i id_rsa ${local.ssh_user}@${aws_instance.default.public_ip}"
+  description = "The command that can be used to login to the instance."
+  value       = "ssh -i id_rsa ${local.ssh_user}@${aws_instance.default.public_ip}"
 }
