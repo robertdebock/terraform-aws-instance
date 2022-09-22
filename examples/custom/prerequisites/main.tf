2b503a79e231686ebf62fd3fb69da98abb202de7
Robert de Bock <robert@meinit.nl>
2022-09-22 15:40:02 +0200

    Create key.

--- examples/custom/prerequisites/main.tf
+++ examples/custom/prerequisites/main.tf
@@ -1,3 +1,18 @@
+resource "tls_private_key" "default" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "default" {
+  key_name   = "centos7"
+  public_key = tls_private_key.default.public_key_openssh
+}
+
+resource "local_file" "default" {
+  filename = "id_rsa"
+  content  = tls_private_key.default.private_key_openssh
+}
+
 module "centos7" {
   source                                     = "../../../"
   instance_distribution                      = "centos7"
