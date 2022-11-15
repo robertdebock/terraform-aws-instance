7a165686ac10cc9897c21a665f368d4e8ff40211
Robert de Bock <robert@meinit.nl>
2022-11-15 15:12:45 +0100

    Add a rule and not on us-east-1e.

--- examples/custom/prerequisites/main.tf
+++ examples/custom/prerequisites/main.tf
@@ -36,8 +36,18 @@ resource "aws_security_group" "default" {
   }
 }
 
+resource "aws_security_group_rule" "ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.default.id
+}
+
 data "aws_availability_zones" "default" {
   state = "available"
+  exclude_names = ["us-east-1e"]
 }
 
 resource "random_shuffle" "default" {
