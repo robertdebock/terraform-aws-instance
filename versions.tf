acc2fa768c72bc597da2e443f4a1fe56fd88918c
Robert de Bock <robert.debock@adfinis.com>
2023-10-05 16:32:46 +0200

    Update providers.

--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = ">= 5.0"
     }
     random = {
       source = "hashicorp/random"
-      version = "3.4.3"
+      version = ">= 3"
     }
   }
 }
